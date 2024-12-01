7065ad0734acdfd684a296b10523ff013dbaf710
Sai-Krishna7 <sai7august@gmail.com>
2024-11-30 17:40:59 -0700

    Updated user-stats end point

--- analytics-dashboard/app.py
+++ analytics-dashboard/app.py
@@ -121,7 +121,7 @@ def get_watch_history_genre():
             cursor.close()
             connection.close()
 
-@app.route('/api/user-stats/<int:user_id>', methods=['GET'])
+@app.route('/api/user-stats/<user_id>', methods=['GET'])
 def get_user_stats(user_id):
     try:
         connection = get_connection()
